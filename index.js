806efae91fcfd4df343d08900218c7a824dfdacb
Betina9 <ob042025@gmail.com>
2025-08-28 14:00:00 +0200

    laget fungerende knapper i js 1 2 3 poeng

---
+++ index.js
@@ -0,0 +1,41 @@
+let onePoint = 1;
+let twoPoints = 2;
+let threePoints = 3;
+let homeScore = 0;
+let guestScore = 0;
+
+function addOnePointHome() {
+  homeScore += onePoint;
+  let homePoints = document.querySelector(".score");
+  homePoints.innerText = homeScore;
+}
+
+function addTwoPointsHome() {
+  homeScore += twoPoints;
+  let homePoints = document.querySelector(".score");
+  homePoints.innerText = homeScore;
+}
+
+function addThreePointsHome() {
+  homeScore += threePoints;
+  let homePoints = document.querySelector(".score");
+  homePoints.innerText = homeScore;
+}
+
+function addOnePointsGuest() {
+  guestScore += onePoint;
+  let guestPoints = document.querySelector(".score2");
+  guestPoints.innerText = guestScore;
+}
+
+function addTwoPointsGuest() {
+  guestScore += twoPoints;
+  let guestPoints = document.querySelector(".score2");
+  guestPoints.innerText = guestScore;
+}
+
+function addThreePointsGuest() {
+  guestScore += threePoints;
+  let guestPoints = document.querySelector(".score2");
+  guestPoints.innerText = guestScore;
+}
